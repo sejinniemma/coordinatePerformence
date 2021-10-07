fcbbc1c4e902831398d3b8d1a4b6ced53bc7d232
Emma <jsj0471@naver.com>
2021-10-07 19:29:24 +0900

    prettier

--- 007.js
+++ 007.js
@@ -1,24 +1,25 @@
 'use strict'
 
-const vertical=document.querySelector('.vertical');
-const horozontal=document.querySelector('.horozontal');
-const target =document.querySelector('.target');
-const tag=document.querySelector('.tag');
+const vertical = document.querySelector('.vertical')
+const horozontal = document.querySelector('.horozontal')
+const target = document.querySelector('.target')
+const tag = document.querySelector('.tag')
 
+addEventListener('load', () => {
+  const targetRect = target.getBoundingClientRect()
+  const targetHalfWidth = targetRect.width / 2
+  const targetHalfHeight = targetRect.height / 2
 
-addEventListener('load',() => {
-    const targetRect =target.getBoundingClientRect();
-    const targetHalfWidth =targetRect.width / 2;
-    const targetHalfHeight =targetRect.height / 2;
+  document.addEventListener('mousemove', (event) => {
+    const x = event.clientX
+    const y = event.clientY
 
-    document.addEventListener('mousemove',(event)=>{
-        const x = event.clientX;
-        const y = event.clientY;
-      
-        vertical.style.transform =`translateX(${x}px)`;
-        horozontal.style.transform =`translateY(${y}px)`;
-        target.style.transform =`translate(${x - targetHalfWidth}px,${y-targetHalfHeight}px)`;
-        tag.style.transform =`translate(${x}px,${y}px)`;
-        tag.innerHTML = `${x}px, ${y}px`
-    })
+    vertical.style.transform = `translateX(${x}px)`
+    horozontal.style.transform = `translateY(${y}px)`
+    target.style.transform = `translate(${x - targetHalfWidth}px,${
+      y - targetHalfHeight
+    }px)`
+    tag.style.transform = `translate(${x}px,${y}px)`
+    tag.innerHTML = `${x}px, ${y}px`
+  })
 })
